9a361d1a4ba014e41db4bf911367617749507614
aroig <aroig@arizona.edu>
2024-02-16 15:47:48 -0700

    prettier fixes

--- src/routes/modifyScholarship/+page.svelte
+++ src/routes/modifyScholarship/+page.svelte
@@ -1,16 +1,16 @@
 <script>
     let areaClicked = {
-        name : false,
-        amount : false,
-        donor : false,
-        phone : false,
-        email : false,
-        numAvailable : false,
-        major : false,
-        minor : false,
-        gpa : false,
-        other : false
-    }
+        name: false,
+        amount: false,
+        donor: false,
+        phone: false,
+        email: false,
+        numAvailable: false,
+        major: false,
+        minor: false,
+        gpa: false,
+        other: false
+    };
 </script>
 
 <a href="/">Home</a>
@@ -21,172 +21,235 @@
     <h1>Modify Scholarship</h1>
 
     <div class="info-container">
-        <div on:click={() => areaClicked.name = !areaClicked.name} class="preview">
+        <div
+            on:click={() => (areaClicked.name = !areaClicked.name)}
+            class="preview">
             <h3>Scholarship Name:</h3>
             <h4>XXXX</h4>
-            <img class="down-arrow" src="/down_arrow.png" alt="down arrow">
+            <img class="down-arrow" src="/down_arrow.png" alt="down arrow" />
         </div>
         {#if areaClicked.name}
-        <div class="input">
-            <input type="text" placeholder="Enter scholarship name here">
-            <div>
-                <button>Confirm</button>
-                <button on:click={() => areaClicked.name = !areaClicked.name}>Cancel</button>
+            <div class="input">
+                <input type="text" placeholder="Enter scholarship name here" />
+                <div>
+                    <button>Confirm</button>
+                    <button
+                        on:click={() => (areaClicked.name = !areaClicked.name)}
+                        >Cancel</button>
+                </div>
             </div>
-        </div>
         {/if}
     </div>
 
     <div class="info-container">
-        <div on:click={() => areaClicked.amount = !areaClicked.amount} class="preview">
+        <div
+            on:click={() => (areaClicked.amount = !areaClicked.amount)}
+            class="preview">
             <h3>Scholarship Amount:</h3>
             <h4>XXXX</h4>
-            <img class="down-arrow" src="/down_arrow.png" alt="down arrow">
+            <img class="down-arrow" src="/down_arrow.png" alt="down arrow" />
         </div>
         {#if areaClicked.amount}
-        <div class="input">
-            <input type="number" placeholder="Enter scholarship amount here">
-            <div>
-                <button>Confirm</button>
-                <button on:click={() => areaClicked.amount = !areaClicked.amount}>Cancel</button>
+            <div class="input">
+                <input
+                    type="number"
+                    placeholder="Enter scholarship amount here" />
+                <div>
+                    <button>Confirm</button>
+                    <button
+                        on:click={() =>
+                            (areaClicked.amount = !areaClicked.amount)}
+                        >Cancel</button>
+                </div>
             </div>
-        </div>
         {/if}
     </div>
 
     <div class="info-container">
-        <div on:click={() => areaClicked.donor = !areaClicked.donor} class="preview">
+        <div
+            on:click={() => (areaClicked.donor = !areaClicked.donor)}
+            class="preview">
             <h3>Scholarship Donor:</h3>
             <h4>XXXX@arizona.edu</h4>
-            <img class="down-arrow" src="/down_arrow.png" alt="down arrow">
+            <img class="down-arrow" src="/down_arrow.png" alt="down arrow" />
         </div>
         {#if areaClicked.donor}
-        <div class="input">
-            <input type="text" placeholder="Enter scholarship donor name here">
-            <div>
-                <button>Confirm</button>
-                <button on:click={() => areaClicked.donor = !areaClicked.donor}>Cancel</button>
+            <div class="input">
+                <input
+                    type="text"
+                    placeholder="Enter scholarship donor name here" />
+                <div>
+                    <button>Confirm</button>
+                    <button
+                        on:click={() =>
+                            (areaClicked.donor = !areaClicked.donor)}
+                        >Cancel</button>
+                </div>
             </div>
-        </div>
         {/if}
     </div>
 
     <div class="info-container">
-        <div on:click={() => areaClicked.phone = !areaClicked.phone} class="preview">
+        <div
+            on:click={() => (areaClicked.phone = !areaClicked.phone)}
+            class="preview">
             <h3>Donor Phone Number:</h3>
             <h4>XXX-XXX-XXXX</h4>
-            <img class="down-arrow" src="/down_arrow.png" alt="down arrow">
+            <img class="down-arrow" src="/down_arrow.png" alt="down arrow" />
         </div>
         {#if areaClicked.phone}
-        <div class="input">
-            <input type="text" placeholder="Enter scholarship donor's phone number here">
-            <div>
-                <button>Confirm</button>
-                <button on:click={() => areaClicked.phone = !areaClicked.phone}>Cancel</button>
+            <div class="input">
+                <input
+                    type="text"
+                    placeholder="Enter scholarship donor's phone number here" />
+                <div>
+                    <button>Confirm</button>
+                    <button
+                        on:click={() =>
+                            (areaClicked.phone = !areaClicked.phone)}
+                        >Cancel</button>
+                </div>
             </div>
-        </div>
         {/if}
     </div>
 
     <div class="info-container">
-        <div on:click={() => areaClicked.email = !areaClicked.email} class="preview">
+        <div
+            on:click={() => (areaClicked.email = !areaClicked.email)}
+            class="preview">
             <h3>Donor Email Address:</h3>
             <h4>XXXX</h4>
-            <img class="down-arrow" src="/down_arrow.png" alt="down arrow">
+            <img class="down-arrow" src="/down_arrow.png" alt="down arrow" />
         </div>
         {#if areaClicked.email}
-        <div class="input">
-            <input type="text" placeholder="Enter scholarship donor's email here">
-            <div>
-                <button>Confirm</button>
-                <button on:click={() => areaClicked.email = !areaClicked.email}>Cancel</button>
+            <div class="input">
+                <input
+                    type="text"
+                    placeholder="Enter scholarship donor's email here" />
+                <div>
+                    <button>Confirm</button>
+                    <button
+                        on:click={() =>
+                            (areaClicked.email = !areaClicked.email)}
+                        >Cancel</button>
+                </div>
             </div>
-        </div>
         {/if}
     </div>
 
     <div class="info-container">
-        <div on:click={() => areaClicked.numAvailable = !areaClicked.numAvailable} class="preview">
+        <div
+            on:click={() =>
+                (areaClicked.numAvailable = !areaClicked.numAvailable)}
+            class="preview">
             <h3>Number of Scholarships Available:</h3>
             <h4>XXXX</h4>
-            <img class="down-arrow" src="/down_arrow.png" alt="down arrow">
+            <img class="down-arrow" src="/down_arrow.png" alt="down arrow" />
         </div>
         {#if areaClicked.numAvailable}
-        <div class="input">
-            <input type="text" placeholder="Enter number of scholarships available here">
-            <div>
-                <button>Confirm</button>
-                <button on:click={() => areaClicked.numAvailable = !areaClicked.numAvailable}>Cancel</button>
+            <div class="input">
+                <input
+                    type="text"
+                    placeholder="Enter number of scholarships available here" />
+                <div>
+                    <button>Confirm</button>
+                    <button
+                        on:click={() =>
+                            (areaClicked.numAvailable =
+                                !areaClicked.numAvailable)}>Cancel</button>
+                </div>
             </div>
-        </div>
         {/if}
     </div>
 
     <div class="info-container">
-        <div on:click={() => areaClicked.major = !areaClicked.major} class="preview">
+        <div
+            on:click={() => (areaClicked.major = !areaClicked.major)}
+            class="preview">
             <h3>Required Majors:</h3>
             <h4>XXXX</h4>
-            <img class="down-arrow" src="/down_arrow.png" alt="down arrow">
+            <img class="down-arrow" src="/down_arrow.png" alt="down arrow" />
         </div>
         {#if areaClicked.major}
-        <div class="input">
-            <input type="text" placeholder="Enter required majors here">
-            <div>
-                <button>Confirm</button>
-                <button on:click={() => areaClicked.major = !areaClicked.major}>Cancel</button>
+            <div class="input">
+                <input type="text" placeholder="Enter required majors here" />
+                <div>
+                    <button>Confirm</button>
+                    <button
+                        on:click={() =>
+                            (areaClicked.major = !areaClicked.major)}
+                        >Cancel</button>
+                </div>
             </div>
-        </div>
         {/if}
     </div>
 
     <div class="info-container">
-        <div on:click={() => areaClicked.minor = !areaClicked.minor} class="preview">
+        <div
+            on:click={() => (areaClicked.minor = !areaClicked.minor)}
+            class="preview">
             <h3>Required Minors:</h3>
             <h4>XXXX</h4>
-            <img class="down-arrow" src="/down_arrow.png" alt="down arrow">
+            <img class="down-arrow" src="/down_arrow.png" alt="down arrow" />
         </div>
         {#if areaClicked.minor}
-        <div class="input">
-            <input type="text" placeholder="Enter required minors here">
-            <div>
-                <button>Confirm</button>
-                <button on:click={() => areaClicked.minor = !areaClicked.minor}>Cancel</button>
+            <div class="input">
+                <input type="text" placeholder="Enter required minors here" />
+                <div>
+                    <button>Confirm</button>
+                    <button
+                        on:click={() =>
+                            (areaClicked.minor = !areaClicked.minor)}
+                        >Cancel</button>
+                </div>
             </div>
-        </div>
         {/if}
     </div>
 
     <div class="info-container">
-        <div on:click={() => areaClicked.gpa = !areaClicked.gpa} class="preview">
+        <div
+            on:click={() => (areaClicked.gpa = !areaClicked.gpa)}
+            class="preview">
             <h3>Required GPA:</h3>
             <h4>XXXX</h4>
-            <img class="down-arrow" src="/down_arrow.png" alt="down arrow">
+            <img class="down-arrow" src="/down_arrow.png" alt="down arrow" />
         </div>
         {#if areaClicked.gpa}
-        <div class="input">
-            <input type="text" placeholder="Enter minimum required GPA here">
-            <div>
-                <button>Confirm</button>
-                <button on:click={() => areaClicked.gpa = !areaClicked.gpa}>Cancel</button>
+            <div class="input">
+                <input
+                    type="text"
+                    placeholder="Enter minimum required GPA here" />
+                <div>
+                    <button>Confirm</button>
+                    <button
+                        on:click={() => (areaClicked.gpa = !areaClicked.gpa)}
+                        >Cancel</button>
+                </div>
             </div>
-        </div>
         {/if}
     </div>
 
     <div class="info-container">
-        <div on:click={() => areaClicked.other = !areaClicked.other} class="preview">
+        <div
+            on:click={() => (areaClicked.other = !areaClicked.other)}
+            class="preview">
             <h3>Other Scholarship Requirements:</h3>
             <h4>XXXX</h4>
-            <img class="down-arrow" src="/down_arrow.png" alt="down arrow">
+            <img class="down-arrow" src="/down_arrow.png" alt="down arrow" />
         </div>
         {#if areaClicked.other}
-        <div class="input">
-            <input type="text" placeholder="Enter any other scholarship requirements here">
-            <div>
-                <button>Confirm</button>
-                <button on:click={() => areaClicked.other = !areaClicked.other}>Cancel</button>
+            <div class="input">
+                <input
+                    type="text"
+                    placeholder="Enter any other scholarship requirements here" />
+                <div>
+                    <button>Confirm</button>
+                    <button
+                        on:click={() =>
+                            (areaClicked.other = !areaClicked.other)}
+                        >Cancel</button>
+                </div>
             </div>
-        </div>
         {/if}
     </div>
 </section>
@@ -207,7 +270,7 @@
         text-align: center;
     }
     .info-container {
-        background-color:lightgray;
+        background-color: lightgray;
         border-radius: 50px;
         padding: 20px;
         margin: 25px;
@@ -216,14 +279,14 @@
         display: flex;
         align-items: center;
         justify-content: flex-start;
-        background-color:lightgray;
+        background-color: lightgray;
     }
     img {
         width: 50px;
         margin-left: auto;
     }
     h4 {
-        color:dimgrey;
+        color: dimgrey;
         margin-left: 10px;
     }
     .input {
@@ -233,7 +296,7 @@
     }
     .input div {
         display: flex;
-        justify-content:space-evenly;
+        justify-content: space-evenly;
     }
     .input button {
         width: 100px;
