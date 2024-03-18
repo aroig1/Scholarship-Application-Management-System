5dd06ad503d8f2839fa549bcac051dc567cad3d7
aroig <aroig@arizona.edu>
2024-03-17 18:45:45 -0700

    fixed type errors for form handling

--- src/routes/modifyScholarship/+page.server.ts
+++ src/routes/modifyScholarship/+page.server.ts
@@ -1,5 +1,5 @@
 import {updateScholarship} from '$lib/util.ts'
-import type { Scholarship } from '$lib/types.js';
+import type { Major, Minor, Scholarship } from '$lib/types.js';
 
 import type { Actions } from '@sveltejs/kit';
 
@@ -11,16 +11,16 @@ export const actions: Actions = {
         const scholarship : Scholarship = {
             id: data.get('id') as string,
             name: data.get('name') as string,
-            amount: data.get('amount'), // number type
+            amount: Number(data.get('amount') as string),
             donorID: data.get('donorID') as string,
-            numAvailable: data.get('numAvailable'), // number type
-            requiredMajors: data.get('requiredMajors'),
-            requiredMinors: data.get('requiredMinors'),
-            requiredGPA: data.get('requiredGPA'),
-            deadline: data.get('deadline'),
+            numAvailable: Number(data.get('numAvailable') as string),
+            requiredMajors: [data.get('requiredMajors') as Major],
+            requiredMinors: [data.get('requiredMinors') as Minor],
+            requiredGPA: Number(data.get('requiredGPA') as string),
+            deadline: new Date(data.get('deadline') as string),
             other: data.get('other') as string
-        }
+        };
 
-        updateScholarship(db, scholarship) // utils function
+        updateScholarship(db, scholarship); // utils function
 	}
 };
