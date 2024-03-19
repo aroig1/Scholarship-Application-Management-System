8be39835497af120a2ea4986f41a17b6bdc15d87
aroig <aroig@arizona.edu>
2024-03-18 19:20:34 -0700

    console logging working

--- src/routes/modifyScholarship/+page.server.ts
+++ src/routes/modifyScholarship/+page.server.ts
@@ -1,26 +1,32 @@
-import {updateScholarship} from '$lib/util.ts'
-import type { Major, Minor, Scholarship } from '$lib/types.js';
+// import {updateScholarship} from '$lib/util.ts'
+import type {Major, Minor, Scholarship} from "$lib/types.js";
 
-import type { Actions } from '@sveltejs/kit';
+import type {Actions} from "@sveltejs/kit";
 
 export const actions: Actions = {
-    default: async ({ cookies, request, platform }) => {
-		const data = await request.formData();
+    default: async ({cookies, request, platform}) => {
+        const data = await request.formData();
         const db = platform?.env.DB;
 
-        const scholarship : Scholarship = {
-            id: data.get('id') as string,
-            name: data.get('name') as string,
-            amount: Number(data.get('amount') as string),
-            donorID: data.get('donorID') as string,
-            numAvailable: Number(data.get('numAvailable') as string),
-            requiredMajors: [data.get('requiredMajors') as Major],
-            requiredMinors: [data.get('requiredMinors') as Minor],
-            requiredGPA: Number(data.get('requiredGPA') as string),
-            deadline: new Date(data.get('deadline') as string),
-            other: data.get('other') as string
+        console.log(data);
+
+        const scholarship: Scholarship = {
+            id: data.get("id") as string,
+            name: data.get("name") as string,
+            amount: Number(data.get("amount") as string),
+            donorID: data.get("donorID") as string,
+            numAvailable: Number(data.get("numAvailable") as string),
+            requiredMajors: [data.get("requiredMajors") as Major],
+            requiredMinors: [data.get("requiredMinors") as Minor],
+            requiredGPA: Number(data.get("requiredGPA") as string),
+            deadline: new Date(data.get("deadline") as string),
+            other: data.get("other") as string
         };
 
-        updateScholarship(db, scholarship); // utils function
-	}
+        // updateScholarship(db, scholarship); // utils function
+
+        return {
+            success: true
+        };
+    }
 };
