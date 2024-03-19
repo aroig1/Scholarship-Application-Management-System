aeffd480b1df5ccd15171a35a5bef52751588118
aroig <aroig@arizona.edu>
2024-03-18 19:20:34 -0700

    console logging working

--- src/routes/studentApplication/+page.server.ts
+++ src/routes/studentApplication/+page.server.ts
@@ -1,19 +1,25 @@
-import {saveApplication} from '$lib/util.ts'
-import type { Application } from '$lib/types.js'
+// import {saveApplication} from '$lib/util.ts'
+import type {Application} from "$lib/types.js";
 
-import type { Actions } from '@sveltejs/kit';
+import type {Actions} from "@sveltejs/kit";
 
 export const actions: Actions = {
-    default: async ({cookies, request, platform} ) => {
-		const data = await request.formData();
+    default: async ({cookies, request, platform}) => {
+        const data = await request.formData();
         const db = platform?.env.DB;
 
-        const application : Application = {
-            applicant: data.get('applicant') as string,
-            scholarship: data.get('scholarship') as string,
-            statement: data.get('statement') as string
+        console.log(data);
+
+        const application: Application = {
+            applicant: data.get("applicant") as string,
+            scholarship: data.get("scholarship") as string,
+            statement: data.get("statement") as string
         };
 
-        saveApplication(db, application); // utils function
-	}
+        // saveApplication(db, application); // utils function
+
+        return {
+            success: true
+        };
+    }
 };
