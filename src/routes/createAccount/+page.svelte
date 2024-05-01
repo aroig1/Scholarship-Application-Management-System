827a38b704b52b2cb3198cea21950f42b4073626
dshay <dorianshay0806@arizona.edu>
2024-04-30 23:59:43 -0700

    Styling

--- src/routes/createAccount/+page.svelte
+++ src/routes/createAccount/+page.svelte
@@ -23,138 +23,137 @@
     let secQuestion2 = "Choose a question";
 </script>
 
-<form use:form2 method="POST">
-    <div class="info-container">
-        <h1>Create Account</h1>
-        <div class="center">
-            <h3>Username</h3>
-            <input
-                class="username"
-                type="text"
-                placeholder="netID"
-                maxlength="31"
-                name="username"
-                required
-                autocomplete="off"
-                use:validators={[required, maxLength(32)]} />
-            <Hint for="username" on="required">This is a mandatory field</Hint>
-        </div>
-        <div class="center">
-            <h3>Password</h3>
-            <input
-                class="password"
-                type="text"
-                placeholder="must include number and special character"
-                maxlength="255"
-                name="password"
-                use:validators={[required, maxLength(255)]} />
-            <Hint for="password" on="required">This is a mandatory field</Hint>
-        </div>
-        <div class="center">
-            <h3>Full Name</h3>
-            <input
-                type="text"
-                placeholder="first name"
-                maxlength="15"
-                name="firstName"
-                use:validators={[required, maxLength(30)]} />
-            <Hint for="firstName" on="required">This is a mandatory field</Hint>
-            <input
-                type="text"
-                placeholder="last name"
-                maxlength="15"
-                name="lastName"
-                use:validators={[required, maxLength(30)]} />
-            <Hint for="lastName" on="required">This is a mandatory field</Hint>
-        </div>
-        <div class="center">
-            <h3>Phone Number</h3>
-            <input
-                type="text"
-                placeholder="(999) 999-9999"
-                maxlength="20"
-                name="phoneNumber"
-                use:validators={[required, maxLength(30)]} />
-            <Hint for="phoneNumber" on="required"
-                >This is a mandatory field</Hint>
-        </div>
-        <div class="center">
-            <h3>Email address</h3>
-            <input
-                type="email"
-                placeholder="netID@arizona.edu"
-                maxlength="25"
-                name="email"
-                use:validators={[required, email, maxLength(30)]} />
-            <HintGroup for="email">
-                <Hint on="required">This is a mandatory field</Hint>
-                <Hint on="email" hideWhenRequired>Email is not valid</Hint>
-            </HintGroup>
-        </div>
-        <div class="center">
-            <h3>User Type</h3>
-            <select
-                name="userType"
-                use:validators={[required]}
-                class="userType">
-                <option value="">Select User Type</option>
-                <option value="0">Applicant</option>
-                <option value="1">Admin</option>
-                <option value="2">Donor</option>
-            </select>
-            <Hint for="userType" on="required">This is a mandatory field</Hint>
-        </div>
-        <div class="center">
-            <h3>Security Question 1</h3>
-            <select
-                bind:value={secQuestion1}
-                class="question_answer"
-                use:validators={[required]}>
-                {#each secQuestions as question}
-                    <option value={question}>
-                        {question}
-                    </option>
-                {/each}
-            </select>
-            <Hint for="question_answer" on="required"
-                >This is a mandatory field</Hint>
-            <input
-                class="question_answer"
-                type="text"
-                placeholder="answer"
-                name="seqQ1"
-                use:validators={[required, maxLength(30)]} />
-            <Hint for="seqQ1" on="required">This is a mandatory field</Hint>
-        </div>
-        <div class="center">
-            <h3>Security Question 2</h3>
-            <select
-                bind:value={secQuestion2}
-                class="question_answer"
-                use:validators={[required]}>
-                {#each secQuestions as question}
-                    <option value={question}>
-                        {question}
-                    </option>
-                {/each}
-            </select>
-            <Hint for="question_answer" on="required"
-                >This is a mandatory field</Hint>
-            <input
-                class="question_answer"
-                type="text"
-                placeholder="answer"
-                name="seqQ2"
-                use:validators={[required, maxLength(30)]} />
-            <Hint for="seqQ2" on="required">This is a mandatory field</Hint>
-        </div>
-        <div class="center">
-            <button disabled={!$form2.valid}>Create Account</button>
-        </div>
-        <div class="center">
-            {#if form?.error}<pre class="error">{form?.message}</pre>{/if}
-        </div>
+<form class="block" use:form2 method="POST">
+    <h1>Create Account</h1>
+    <div class="center">
+        <h3>Username</h3>
+        <input
+            class="username"
+            type="text"
+            placeholder="netID"
+            maxlength="31"
+            name="username"
+            required
+            autocomplete="off"
+            use:validators={[required, maxLength(32)]} />
+        <Hint for="username" on="required">This is a mandatory field</Hint>
     </div>
+    <div class="center">
+        <h3>Password</h3>
+        <input
+            class="password"
+            type="text"
+            placeholder="must include number and special character"
+            maxlength="255"
+            name="password"
+            use:validators={[required, maxLength(255)]} />
+        <Hint for="password" on="required">This is a mandatory field</Hint>
+    </div>
+    <div class="center">
+        <h3>Full Name</h3>
+        <input
+            type="text"
+            placeholder="first name"
+            maxlength="15"
+            name="firstName"
+            use:validators={[required, maxLength(30)]} />
+        <Hint for="firstName" on="required">This is a mandatory field</Hint>
+        <input
+            type="text"
+            placeholder="last name"
+            maxlength="15"
+            name="lastName"
+            use:validators={[required, maxLength(30)]} />
+        <Hint for="lastName" on="required">This is a mandatory field</Hint>
+    </div>
+    <div class="center">
+        <h3>Phone Number</h3>
+        <input
+            type="text"
+            placeholder="(999) 999-9999"
+            maxlength="20"
+            name="phoneNumber"
+            use:validators={[required, maxLength(30)]} />
+        <Hint for="phoneNumber" on="required"
+            >This is a mandatory field</Hint>
+    </div>
+    <div class="center">
+        <h3>Email address</h3>
+        <input
+            type="email"
+            placeholder="netID@arizona.edu"
+            maxlength="25"
+            name="email"
+            use:validators={[required, email, maxLength(30)]} />
+        <HintGroup for="email">
+            <Hint on="required">This is a mandatory field</Hint>
+            <Hint on="email" hideWhenRequired>Email is not valid</Hint>
+        </HintGroup>
+    </div>
+    <div class="center">
+        <h3>User Type</h3>
+        <select
+            name="userType"
+            use:validators={[required]}
+            class="userType">
+            <option value="">Select User Type</option>
+            <option value="0">Applicant</option>
+            <option value="1">Admin</option>
+            <option value="2">Donor</option>
+        </select>
+        <Hint for="userType" on="required">This is a mandatory field</Hint>
+    </div>
+    <div class="center">
+        <h3>Security Question 1</h3>
+        <select
+            bind:value={secQuestion1}
+            class="question_answer"
+            use:validators={[required]}>
+            {#each secQuestions as question}
+                <option value={question}>
+                    {question}
+                </option>
+            {/each}
+        </select>
+        <Hint for="question_answer" on="required"
+            >This is a mandatory field</Hint>
+        <input
+            class="question_answer"
+            type="text"
+            placeholder="answer"
+            name="seqQ1"
+            use:validators={[required, maxLength(30)]} />
+        <Hint for="seqQ1" on="required">This is a mandatory field</Hint>
+    </div>
+    <div class="center">
+        <h3>Security Question 2</h3>
+        <select
+            bind:value={secQuestion2}
+            class="question_answer"
+            use:validators={[required]}>
+            {#each secQuestions as question}
+                <option value={question}>
+                    {question}
+                </option>
+            {/each}
+        </select>
+        <Hint for="question_answer" on="required"
+            >This is a mandatory field</Hint>
+        <input
+            class="question_answer"
+            type="text"
+            placeholder="answer"
+            name="seqQ2"
+            use:validators={[required, maxLength(30)]} />
+        <Hint for="seqQ2" on="required">This is a mandatory field</Hint>
+    </div>
+    <div class="center">
+        <button disabled={!$form2.valid}>Create Account</button>
+    </div>
+    <div class="center">
+        {#if form?.error}<pre class="error">{form?.message}</pre>{/if}
+    </div>
+    
 </form>
 
 <style>
@@ -162,23 +161,28 @@
         border-color: red;
         outline-color: red;
     }
+    
     h1 {
         width: 100%;
         text-align: center;
     }
-    h3 {
-        padding: 0;
-        margin: 0;
-    }
+
     div {
         padding: 0;
         margin: 20px;
     }
     button {
-        /* figure out how to center */
-        margin: 0 auto;
-        padding: 10px;
-        width: 150px;
+        margin-top: 10px;
+        padding: 10px 25px;
+        border-radius: 25px;
+        border-style: none;
+        background-color: rgb(13, 35, 75);
+        color: white;
+    }
+
+    button:hover {
+        background-color: rgb(55, 141, 189);
+        cursor: pointer;
     }
 
     .username,
@@ -200,5 +204,52 @@
         flex-direction: column;
         text-align: center;
         padding: 2px;
+    }
+
+    body {
+    margin: 0;
+    padding: 0;
+    background-color: #000;
+    }
+
+    .block {
+        position: relative;
+        margin: 50;
+        margin: 25px 25%;
+        padding: 35px;
+        /* width: 500px;
+        height: 250px; */
+        background-color: white;
+        border-radius: 50px;
+    }
+
+    .block:before, .block:after {
+        content: '';
+        position: absolute;
+        left: -2px;
+        top: -2px;
+        background: linear-gradient(45deg, #d8bfd8, #ffb6c1, #9370db, #ff1493, #add8e6, #00008b, #0000cd);
+        background-size: 400%;
+        width: calc(100% + 4px);
+        height: calc(100% + 4px);
+        z-index: -1;
+        animation: steam 20s linear infinite;
+        border-radius: 50px;
+    }
+
+    @keyframes steam {
+    0% {
+        background-position: 0 0;
+    }
+    50% {
+        background-position: 400% 0;
+    }
+    100% {
+        background-position: 0 0;
+    }
+    }
+
+    .block:after {
+    filter: blur(50px);
     }
 </style>
--- src/routes/loginPage/+page.svelte
+++ src/routes/loginPage/+page.svelte
@@ -11,45 +11,49 @@
     const form2 = useForm();
 </script>
 
-<form use:form2 method="post" action="?/login">
-    <div class="info-container">
+<body>
+<form class="login" use:form2 method="post" action="?/login">
+    <!-- <div class="info-container"> -->
         <h1>Login</h1>
 
-        <div class="center">
+        <!-- <div class="center"> -->
             <input
                 class="username"
                 type="text"
+                placeholder="Username"
                 maxLength="31"
                 name="username"
                 required
                 autocomplete="off"
                 use:validators={[required, maxLength(31)]} />
             <Hint for="username" on="required">This is a mandatory field</Hint>
-        </div>
+        <!-- </div> -->
 
-        <div class="center">
+        <!-- <div class="center"> -->
             <input
                 type="password"
+                placeholder="password"
                 name="password"
                 maxLength="255"
                 use:validators={[required, maxLength(255)]} />
             <Hint for="password" on="required">This is a mandatory field</Hint>
-        </div>
+        <!-- </div> -->
 
-        <div class="center">
+        <!-- <div class="center"> -->
             <button disabled={!$form2.valid}>Login</button>
-        </div>
+        <!-- </div> -->
 
-        <div class="center">
-            <a href="/createAccount"><button>Create Account</button></a>
-        </div>
+        <!-- <div class="center"> -->
+            <a href="/createAccount"><button>Sign Up</button></a>
+        <!-- </div> -->
         {#if form?.error}
-            <div class="center">
+            <!-- <div class="center"> -->
                 <pre class="error">{form?.message}</pre>
-            </div>
+            <!-- </div> -->
         {/if}
-    </div>
+    <!-- </div> -->
 </form>
+</body>
 
 <style>
     :global(.touched:invalid) {
@@ -57,25 +61,115 @@
         outline-color: red;
     }
 
-    * {
-        padding: 0px;
-        margin: 0px;
+    .login {
+        overflow: hidden;
+        background-color: white;
+        padding: 60px 30px 50px 30px;
+        border-radius: 10px;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        width: 500px;
+        -webkit-transform: translate(-50%, -50%);
+        -moz-transform: translate(-50%, -50%);
+        -ms-transform: translate(-50%, -50%);
+        -o-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+        -webkit-transition: -webkit-transform 300ms, box-shadow 300ms;
+        -moz-transition: -moz-transform 300ms, box-shadow 300ms;
+        transition: transform 300ms, box-shadow 300ms;
+        box-shadow: 5px 10px 10px rgba(2, 128, 144, 0.2);
+    }
+    .login::before, .login::after {
+        content: "";
+        position: absolute;
+        width: 600px;
+        height: 600px;
+        border-top-left-radius: 40%;
+        border-top-right-radius: 45%;
+        border-bottom-left-radius: 35%;
+        border-bottom-right-radius: 40%;
+        z-index: -1;
+    }
+    .login::before {
+        left: 40%;
+        bottom: -130%;
+        background-color: rgba(69, 105, 144, 0.15);
+        -webkit-animation: wawes 6s infinite linear;
+        -moz-animation: wawes 6s infinite linear;
+        animation: wawes 6s infinite linear;
+    }
+    .login::after {
+        left: 35%;
+        bottom: -125%;
+        background-color: rgba(2, 128, 144, 0.2);
+        -webkit-animation: wawes 7s infinite;
+        -moz-animation: wawes 7s infinite;
+        animation: wawes 7s infinite;
+    }
+    .login > input {
+        font-family: "Asap", sans-serif;
+        display: block;
+        border-radius: 5px;
+        font-size: 16px;
+        background: white;
+        width: 100%;
+        border: 0;
+        padding: 10px 10px;
+        margin: 15px -10px;
+    }
+    .login > button, a button {
+        font-family: "Asap", sans-serif;
+        cursor: pointer;
+        color: #fff;
+        font-size: 16px;
+        text-transform: uppercase;
+        width: 80px;
+        border: 0;
+        padding: 10px 0;
+        margin-right: 25px;
+        margin-top: 10px;
+        margin-left: -5px;
+        border-radius: 5px;
+        background-color: #f45b69;
+        -webkit-transition: background-color 300ms;
+        -moz-transition: background-color 300ms;
+        transition: background-color 300ms;
+    }
+    .login > button:hover, a button {
+        background-color: #f24353;
     }
 
-    .info-container {
-        background-color: lightgray;
-        border: 50px;
-        padding: 20px;
-        margin: 25px;
-        width: 400px;
-        height: 200px;
-        flex-direction: column;
-        text-align: center;
-    }
-
-    .center {
-        flex-direction: column;
-        text-align: center;
-        padding: 2px;
+    @-webkit-keyframes wawes {
+        from {
+            -webkit-transform: rotate(0);
+        }
+        to {
+            -webkit-transform: rotate(360deg);
+        }
+        }
+        @-moz-keyframes wawes {
+        from {
+            -moz-transform: rotate(0);
+        }
+        to {
+            -moz-transform: rotate(360deg);
+        }
+        }
+        @keyframes wawes {
+        from {
+            -webkit-transform: rotate(0);
+            -moz-transform: rotate(0);
+            -ms-transform: rotate(0);
+            -o-transform: rotate(0);
+            transform: rotate(0);
+        }
+        to {
+            -webkit-transform: rotate(360deg);
+            -moz-transform: rotate(360deg);
+            -ms-transform: rotate(360deg);
+            -o-transform: rotate(360deg);
+            transform: rotate(360deg);
+        }
     }
 </style>
